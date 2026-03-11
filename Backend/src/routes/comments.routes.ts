2f8aa2fc056024206339713aa5ad4a4951402879
khonchanphearaa007 <khonchanphearaa@gmail.com>
2026-03-11 13:14:17 +0700

    update: routes add liked

--- Backend/src/routes/comments.routes.ts
+++ Backend/src/routes/comments.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { getComments, createComment, deleteComment, updateComment } from '../controllers/commentController'
+import { getComments, createComment, updateComment, deleteComment, toggleLike, } from '../controllers/commentController';
 import { protect } from '../middlewares/auth';
 
 const router = Router();
@@ -8,5 +8,6 @@ router.get('/:articleId', getComments);
 router.post('/:articleId', protect, createComment);
 router.put('/:commentId', protect, updateComment);
 router.delete('/:commentId', protect, deleteComment);
+router.post('/:commentId/like', protect, toggleLike);
 
 export default router;
